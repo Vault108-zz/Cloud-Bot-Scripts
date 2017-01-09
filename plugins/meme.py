0dec9f12ed6446f3dee53d9ce8b8ac01951da8ca
Tyler <Vault108@users.noreply.github.com>
2017-01-09 02:03:48 -0500

    Update meme.py

--- plugins/meme.py
+++ plugins/meme.py
@@ -8,7 +8,7 @@ from cloudbot import hook
 
 @hook.on_start()
 def load_meme(bot):
-    path = os.path.join(bot.data_dir, "meme.txt")
+    path = os.path.join(bot.vault_dir, "meme.txt")
     global meme
     with codecs.open(path, encoding="utf-8") as f:
         meme = [line.strip() for line in f.readlines() if not line.startswith("//")]
